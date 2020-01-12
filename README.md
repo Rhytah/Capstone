eca223147c714ec3d18191a6988e10a0d76eebc8
jennizalwango <ritanamono@gmail.com>
2020-01-12 23:43:35 +0300

    implement API

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# Capstone
+Capstone
+-----
+
+### Introduction
+
+Capstone is a casting agency site 
+
+
+### Tech Stack
+
+Our tech stack will include:
+
+* **SQLAlchemy ORM** to be our ORM library of choice
+* **PostgreSQL** as our database of choice
+* **Python3** and **Flask** as our server language and server framework
+* **Flask-Migrate** for creating and running schema migrations
+* **HTML**, **CSS**, and **Javascript** with [Bootstrap 3](https://getbootstrap.com/docs/3.4/customize/) for our website's frontend
+
+### Main Files: Project Structure
+
+  ```sh
+  ├── README.md
+  ├── app.py *** the main driver of the app. 
+                    "python app.py" to run after installing dependences
+  ├── config.py *** Database URLs, CSRF generation, etc
+  ├── error.log
+  ├── forms.py *** Your forms
+  ├── models.py  *** Your SQL Alchemy models
+  ├── requirements.txt *** The dependencies we need to install with "pip3 install -r requirements.txt"
+  ├── static
+  │   ├── css 
+  │   ├── font
+  │   ├── ico
+  │   ├── img
+  │   └── js
+  └── templates
+      ├── errors
+      ├── forms
+      ├── layouts
+      └── pages
+  ```
+
+Overall:
+* Models are located in `models.py`.
+* Controllers are located in `app.py`.
+* The web frontend is located in `templates/`, which builds static assets deployed to the web server at `static/`.
+* Web forms for creating data are located in `form.py`
+
+
+### Development Setup
+
+First, [install Flask](http://flask.pocoo.org/docs/1.0/installation/#install-flask) if you haven't already.
+
+  ```
+  $ cd ~
+  $ sudo pip3 install Flask
+  ```
+
+To start and run the local development server,
+
+1. Initialize and activate a virtualenv:
+  ```
+  $ cd YOUR_PROJECT_DIRECTORY_PATH/
+  $ virtualenv --no-site-packages env
+  $ source env/bin/activate
+  ```
+
+2. Install the dependencies:
+  ```
+  $ pip install -r requirements.txt
+  ```
+
+3. Run the development server:
+  ```
+  $ export FLASK_APP=myapp
+  $ export FLASK_ENV=development # enables debug mode
+  $ python3 app.py
+  ```
+
+4. Navigate to Home page [http://localhost:5000](http://localhost:5000)
--- models.py
+++ models.py
@@ -1,1 +1,176 @@
+from datetime import datetime
 
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String())
+    release_date = db.Column(db.DateTime)
+    deleted = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Movie self.title {self.title}>'
+
+    def get_enum(self):
+        return [
+            (movie.id, movie.title)
+            for movie in self.query.with_entities(self.id, self.title).all()
+        ]
+
+    def get_by_id(self, id):
+        return self.query.filter_by(id=id).first_or_404()
+
+    @classmethod
+    def get_by_id_full(cls, id):
+        details = {}
+        movie = cls.get_by_id(id)
+        details.update(movie)
+
+        return details
+
+    @classmethod
+    def name_search(cls, movie_title):
+        movies = cls.query.filter(
+            cls.title.ilike(f'%{movie_title}%')
+        ).all()
+        return [
+            {
+                "id": movie.id,
+                "title": movie.title,
+            }
+            for movie in movies
+        ]
+
+    def exists(self, name):
+        return self.query.filter(db.func.lower(self.title) == db.func.lower(title)).count()
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id).serialize
+
+
+    @classmethod
+    def get_all(cls):
+        movies = cls.query.all()
+        results = [
+            {
+                'title': movie.title,
+                'website': movie.website,
+                'image_link': movie.image_link,
+                'id': movie.id
+            }
+            for movie in movies
+        ]
+
+        return results
+        
+    # @classmethod
+    # def delete_movie(movie_id):
+    #     try:
+    #         movie = Movie.get_by_id(movie_id)
+    #         print(movie)
+    #         db.session.delete(movie)
+    #         db.session.commit()
+    #         print("it has worked")
+    #     except Exception as error:
+    #         db.session.rollback()
+    #         raise error
+    #     finally:
+    #         db.session.close()
+ 
+        
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+            'website': self.website,
+            'facebook_link': self.facebook_link,
+            'image_link': self.image_link
+        }
+
+
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.String(120))
+    gender = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+   
+
+
+
+    def __repr__(self):
+        return f'<Actor self.id {self.name}>'
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id)
+
+    @classmethod
+    def get_by_id_full(cls, id):
+        details = {}
+        actor = cls.get_by_id(id)
+        details.update(actor.serialize)
+
+        return details
+    @classmethod
+    def search_actor_name(cls, actor_name):
+        movies = cls.query.filter(
+            cls.name.ilike(f'%{actor_name}%')
+        ).all()
+        return [
+            {
+                "id": actor.id,
+                "name": actor.name,
+            }
+            for actor in actors
+        ]
+    @classmethod
+    def get_all(cls):
+        actors = cls.query.all()
+        results = [
+            {
+                'name': actor.name,
+                'gender': actor.gender,
+                'age': actor.age,
+                'image_link': actor.image_link,
+                'id': actor.id
+            }
+            for actor in actors
+        ]
+
+        return results
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+            'image_link': self.image_link
+        }
+
+    def __repr__(self):
+        return f'<Actor name: {self.name}>'
+    
