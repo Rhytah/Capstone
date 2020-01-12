7fd0997dc5fa7e6835df6ef27b8a230703abdfee
jennizalwango <ritanamono@gmail.com>
2020-01-12 23:43:35 +0300

    implement API

---
+++ forms.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, AnyOf, URL
+
+
+class MovieForm(Form):
+    title = StringField(
+        'title', validators=[DataRequired()]
+    )
+    website = StringField(
+        'website', validators=[DataRequired()]
+    )
+    release_date = DateTimeField(
+        'release_date', validators=[DataRequired()]
+    )
+    image_link = StringField(
+        'image_link'
+    )
+    facebook_link = StringField(
+        'facebook_link', validators=[URL()]
+    )
+    description = StringField(
+        'description', validators=[DataRequired()]
+    )
+class ActorForm(Form):
+    name = StringField(
+        'name', validators=[DataRequired()]
+    )
+    
+    age = IntegerField(
+        'age', validators=[DataRequired()]
+    )
+    gender = StringField(
+        'gender', validators=[DataRequired()]
+    )
+    image_link = StringField(
+        'image_link'
+    )
